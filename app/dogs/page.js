ba3d1adee9b026f87066c259aeaed8d73ea09f95
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2025-04-02 14:59:03 -0300

    busca por raça

--- app/dogs/page.js
+++ app/dogs/page.js
@@ -10,6 +10,11 @@ function Dogs() {
     async function buscaTodosDogs(){
         const response = await axios.get("https://dog.ceo/api/breeds/image/random/30")
         alteraDogs( response.data.message )
+    }   
+
+    async function buscaPorRaca( raca ){
+        const response = await axios.get("https://dog.ceo/api/breed/"+raca+"/images")
+        alteraDogs( response.data.message )
     }
 
     useEffect(()=> {
@@ -24,6 +29,11 @@ function Dogs() {
 
             <hr/>
 
+            <button onClick={()=> buscaTodosDogs() } className="bg-green-200 text-green-700 m-3 p-3">Mostrar tudo</button>
+            <button onClick={()=> buscaPorRaca("pitbull") } className="bg-green-200 text-green-700 m-3 p-3">Pitbull</button>
+            <button onClick={()=> buscaPorRaca("labrador") } className="bg-green-200 text-green-700 m-3 p-3">Labrador</button>
+            <button onClick={()=> buscaPorRaca("beagle") } className="bg-green-200 text-green-700 m-3 p-3">Beagle</button>
+     
             {
                 dogs.length > 0 ?
                     <div className="flex gap-5 flex-wrap" >
